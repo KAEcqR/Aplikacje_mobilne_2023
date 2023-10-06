d702a0f0e31e1ad10675687c198b59223e2141a3
kacper <kacperprochwicz@op.pl>
2023-10-06 18:24:45 +0200

    cw3

--- Cw1/app/src/main/java/com/example/cw1/Cw1.kt
+++ Cw1/app/src/main/java/com/example/cw1/Cw1.kt
@@ -1,5 +1,7 @@
 package com.example.cw1
 
+import java.util.Scanner
+
 fun main(){
     println("hello world");
     println(wizytowka("kacperek", "ciapek", 15));
@@ -8,3 +10,22 @@ fun main(){
 fun wizytowka(firstname: String, lastname: String, age: Int=18 ):String{
     return "--------------\nimie: $firstname \nnazwisko: $lastname \nwiek: $age \n--------------";
 }
+
+fun test(){
+    try {
+        print("Podaj imie: ")
+        val sc = Scanner(System.`in`);
+        val firstname = sc.next()
+
+        print("Podaj nazwisko: ")
+        val lastname = sc.next()
+
+        print("Podaj Wiek: ")
+        val age = sc.next()
+        
+    }catch (e:Exception){
+        println(e.message)
+    }
+
+}
+
---
+++ Cw2/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+fun main(args: Array<String>) {
+    first()
+}
+
+fun first(){
+    try {
+    
+    }catch (e: Exception){
+        println(e.message)
+    }
+}
