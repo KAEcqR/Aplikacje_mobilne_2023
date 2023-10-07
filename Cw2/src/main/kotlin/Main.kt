172f76739aa622a471aa81be81a1c53a5c98f431
kacper <kacperprochwicz@op.pl>
2023-10-07 08:35:58 +0200

    Cw2

--- Cw2/src/main/kotlin/Main.kt
+++ Cw2/src/main/kotlin/Main.kt
@@ -1,11 +1,21 @@
+import java.util.*
+
 fun main(args: Array<String>) {
-    first()
+    first("tekst", 5)
 }
 
-fun first(){
-    try {
-    
-    }catch (e: Exception){
-        println(e.message)
+var i = 0
+val sc = Scanner(System.`in`);
+
+fun first(tekst: String, num: Int ){
+
+    println("podaj tekst");
+    val tekst = sc.next()
+    println("podaj liczbe");
+    val Int = sc.next()
+
+    while (i < num){
+        println(tekst);
+        i++
     }
 }
