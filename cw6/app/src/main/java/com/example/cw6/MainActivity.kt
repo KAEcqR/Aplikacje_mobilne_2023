d1b0a96c6660eceed1db9896c1b69f3479dbda23
Kacper <kacperprochwicz@op.pl>
2023-10-13 15:41:54 +0200

    cw5
    cw5

---
+++ cw6/app/src/main/java/com/example/cw6/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.cw6
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        val info = findViewById<TextView>(R.id.textView)
+
+        button.setOnClickListener {
+            info.text = "To jest po kliknieciu"
+        }
+    }
+}
